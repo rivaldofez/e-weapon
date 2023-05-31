369428280419a72364bed947795612360cc85750
rivaldofez <rivaldofez@gmail.com>
2023-06-01 01:25:00 +0700

    Initial Commit

---
+++ e-weapon/e_weaponApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_weaponApp.swift
+//  e-weapon
+//
+//  Created by Rivaldo Fernandes on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct e_weaponApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
