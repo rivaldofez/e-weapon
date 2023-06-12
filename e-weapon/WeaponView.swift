2b56db833f2661b4e996359b72da021317901698
rivaldofez <rivaldofez@gmail.com>
2023-06-12 15:27:22 +0700

    add search bar at weapon list

--- e-weapon/WeaponView.swift
+++ e-weapon/WeaponView.swift
@@ -8,44 +8,50 @@
 import SwiftUI
 
 struct WeaponView: View {
+    @State private var searchQuery: String = ""
+    
+    
     var body: some View {
         NavigationView {
             VStack {
                 
             }
+            .searchable(text: self.$searchQuery)
+//            .navigationTitle("Weapon")
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
                     Text("Weapon")
-                        .font(.system(.largeTitle).bold())
+                        .font(.system(.title).bold())
                 }
-                
+
                 ToolbarItem(placement: .navigationBarTrailing) {
                     HStack(alignment: .center, spacing: 0) {
                         Button {
-                            
+
                         } label: {
                             HStack(spacing: 0) {
                                 Image(systemName: "square.and.arrow.up.circle.fill")
                                 Text("Tambah")
-                                
+
                             }
                         }
-                        
+
                         Button {
-                            
+
                         } label: {
                             HStack(spacing: 0) {
                                 Image(systemName: "square.and.arrow.up.circle.fill")
                                 Text("Export")
-                                
+
 
                             }
                         }
                     }
                 }
-                
-                
+
+
             }
+            
         }
         
     }
