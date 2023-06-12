e66ecb63c5f57a52e06d9468ed45ff49e726885d
rivaldofez <rivaldofez@gmail.com>
2023-06-12 22:42:51 +0700

    add weapon entity

---
+++ e-weapon/Local/Entity/WeaponEntity.swift
@@ -0,0 +1,17 @@
+//
+//  WeaponEntity.swift
+//  e-weapon
+//
+//  Created by Rivaldo Fernandes on 12/06/23.
+//
+
+import Foundation
+import RealmSwift
+
+class WeaponEntity: Object {
+    @Persisted var id: String
+    @Persisted var name: String
+    @Persisted var addedAt: Date
+    @Persisted var price : Double
+    @Persisted var stock: Int
+}
