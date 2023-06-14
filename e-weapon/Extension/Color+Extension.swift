043a351d94c2b3dbad7bc671d24bb57ab1d79fcb
rivaldofez <rivaldofez@gmail.com>
2023-06-14 11:30:54 +0700

    add color and extension color

---
+++ e-weapon/Extension/Color+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  Color+Extension.swift
+//  e-weapon
+//
+//  Created by Rivaldo Fernandes on 14/06/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryAccent = Color("MyrticGreen")
+    static let secondaryAccent = Color("Coral")
+    
+    static let primaryLabel = Color("Jet")
+    static let primaryButtonLabel = Color("Platinum")
+    static let primaryGray = Color("Timberwolf")
+    
+    static let darkGold = Color("DarkGold")
+    static let darkSilver = Color("DarkSilver")
+    static let darkBronze = Color("DarkBronze")
+    
+    static let gold = Color("Gold")
+    static let silver = Color("Silver")
+    static let bronze = Color("Bronze")
+}
