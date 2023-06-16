8d04ebc104d2dfd12ea293c2af6d0522453120e4
rivaldofez <rivaldofez@gmail.com>
2023-06-17 01:32:04 +0700

    add contants of location and status options

---
+++ e-weapon/Utils/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  e-weapon
+//
+//  Created by Rivaldo Fernandes on 17/06/23.
+//
+
+import Foundation
+
+//pusat, daerah, lapangan, gudang, bengkel
+
+
+class Constants {
+    static let locationOptions: [String] = [
+        "Center",
+        "District",
+        "Field",
+        "Warehouse",
+        "Workshop"
+    ]
+    
+    static let statusOptions: [String] = [
+        "Stored",
+        "In Use"
+    ]
+}
+
