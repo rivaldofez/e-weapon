6fc221bf390ea435c92120500eb6adda352a110b
rivaldofez <rivaldofez@gmail.com>
2023-06-16 18:35:44 +0700

    fixing minor bug on update main view

--- e-weapon/Page/WeaponList/WeaponViewModel.swift
+++ e-weapon/Page/WeaponList/WeaponViewModel.swift
@@ -15,17 +15,11 @@ class WeaponViewModel: ObservableObject {
     @Published var weapon: [Weapon] = []
     
     func fetchWeapon(){
-        print("before")
-        print(weapon)
-        
-        self.weapon.removeAll()
+
         self.weapon = databaseManager.fetchWeapon()
             .map {
                 return Weapon(id: $0.id, name: $0.name, addedAt: $0.addedAt, price: $0.price, stock: $0.stock, imageUrl: $0.imageUrl, location: $0.location, status: $0.status)
             }
-        
-        print("after")
-        print(weapon)
     }
     
     func deleteWeapon(id: String, completion: @escaping (Result<Void, Error>) -> Void){
