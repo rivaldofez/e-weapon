43bfe6193ed0307aad9d0e0bfd8a340bbdff7f64
rivaldofez <rivaldofez@gmail.com>
2023-06-17 14:23:45 +0700

    implement accessory item view

--- e-weapon/Page/AccessoryList/AccessoryItemView.swift
+++ e-weapon/Page/AccessoryList/AccessoryItemView.swift
@@ -8,13 +8,76 @@
 import SwiftUI
 
 struct AccessoryItemView: View {
+    @Binding var accessory: Accessory
+
+    func getImage(imageUrl: String) -> UIImage {
+        let imagesDefaultURL = URL(fileURLWithPath: "/images/")
+        let imagesFolderUrl = try! FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: imagesDefaultURL, create: true)
+        let imageUrl = imagesFolderUrl.appendingPathComponent(imageUrl)
+        
+        do {
+            let imageData = try Data(contentsOf: imageUrl)
+            
+            if let imageResult = UIImage(data: imageData){
+                return imageResult
+            }
+        } catch {
+            print("Not able to load image")
+        }
+        
+        return UIImage(systemName: "exclamationmark.triangle.fill")!
+        
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .center) {
+            Image(uiImage: getImage(imageUrl: accessory.imageUrl))
+                .resizable()
+                .frame(width: 80, height: 80)
+                .cornerRadius(16)
+                .padding(.trailing, 16)
+                
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(accessory.name)
+                    .font(.system(.title3).bold())
+                    .foregroundColor(.primaryAccent)
+                
+                Text("\(accessory.price)")
+                    .font(.system(.body).bold())
+                    .foregroundColor(.secondaryAccent)
+                
+                HStack {
+                    HStack {
+                        Image(systemName: "shippingbox.fill")
+                            .font(.system(.callout))
+                            .foregroundColor(.primaryGray)
+                        Text("\(accessory.stock)")
+                            .font(.system(.callout))
+                            .foregroundColor(.primaryGray)
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        HStack {
+                            Image(systemName: "info.square.fill")
+                                .font(.system(.callout))
+                                .foregroundColor(.primaryGray)
+                            Text(accessory.status)
+                                .font(.system(.callout))
+                                .foregroundColor(.primaryGray)
+                            Spacer()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 struct AccessoryItemView_Previews: PreviewProvider {
     static var previews: some View {
-        AccessoryItemView()
+        AccessoryItemView(accessory: .constant(Accessory(id: "", name: "Palu Gada", addedAt: Date(), price: 5000, stock: 100, imageUrl: "", location: "Gudang", status: "Save")))
     }
 }
+
