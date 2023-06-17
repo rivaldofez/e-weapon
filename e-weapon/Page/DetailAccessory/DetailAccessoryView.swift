4077e09503dced66604fe96f99b962c2dfd63cd0
rivaldofez <rivaldofez@gmail.com>
2023-06-17 15:11:17 +0700

    add detail accessory view and detail accessory view model

---
+++ e-weapon/Page/DetailAccessory/DetailAccessoryView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailAccessoryView.swift
+//  e-weapon
+//
+//  Created by Rivaldo Fernandes on 17/06/23.
+//
+
+import SwiftUI
+
+struct DetailAccessoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailAccessoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailAccessoryView()
+    }
+}
---
+++ e-weapon/Page/DetailAccessory/DetailAccessoryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailAccessoryViewModel.swift
+//  e-weapon
+//
+//  Created by Rivaldo Fernandes on 17/06/23.
+//
+
+import Foundation
