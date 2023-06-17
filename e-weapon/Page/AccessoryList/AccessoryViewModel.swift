263f5fb5f9781ca08910ceb8297b7d71d74e545d
rivaldofez <rivaldofez@gmail.com>
2023-06-17 14:26:46 +0700

    implement fetch data accessories on view model

--- e-weapon/Page/AccessoryList/AccessoryViewModel.swift
+++ e-weapon/Page/AccessoryList/AccessoryViewModel.swift
@@ -5,4 +5,20 @@
 //  Created by Rivaldo Fernandes on 17/06/23.
 //
 
-import Foundation
+import UIKit
+
+class AccessoryViewModel: ObservableObject {
+    
+    private let databaseManager = DatabaseManager.shared
+    
+    @Published var accessories: [Accessory] = []
+    
+    func fetchAccessory(){
+        
+        self.accessories = databaseManager.fetchAccessory()
+            .map {
+                return Accessory(id: $0.id, name: $0.name, addedAt: $0.addedAt, price: $0.price, stock: $0.stock, imageUrl: $0.imageUrl, location: $0.location, status: $0.status)
+            }
+    }
+    
+}
