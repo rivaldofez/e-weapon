d0c93cb275e4432f8bc0eec685cc4f451b25bb8d
rivaldofez <rivaldofez@gmail.com>
2023-06-17 14:16:53 +0700

    add accessory list view

---
+++ e-weapon/Page/AccessoryList/AccessoryView.swift
@@ -0,0 +1,20 @@
+//
+//  AccessoryView.swift
+//  e-weapon
+//
+//  Created by Rivaldo Fernandes on 17/06/23.
+//
+
+import SwiftUI
+
+struct AccessoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccessoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccessoryView()
+    }
+}
