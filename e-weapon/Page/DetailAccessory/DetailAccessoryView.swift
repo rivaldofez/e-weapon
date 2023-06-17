26b7a60dd0ab42184d3a2f24dfe5fb7f6c05e6f1
rivaldofez <rivaldofez@gmail.com>
2023-06-17 15:23:51 +0700

    implemen view and update method on detail accessory view

--- e-weapon/Page/DetailAccessory/DetailAccessoryView.swift
+++ e-weapon/Page/DetailAccessory/DetailAccessoryView.swift
@@ -8,13 +8,209 @@
 import SwiftUI
 
 struct DetailAccessoryView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var showAlert: Bool = false
+    @State private var alertMessage: String = ""
+    @State private var alertTitle: String = ""
+    
+    
+    var id: String
+    var imageUrl: String
+    var addedAt: Date
+    @State var name: String = ""
+    @State var price: String = ""
+    @State var stock: String = ""
+    @State var currentImage: UIImage? = nil
+    
+    
+    @State private var showImageActionDialog: Bool = false
+    @State private var sourceType: UIImagePickerController.SourceType = .camera
+    @State private var showImagePickerSheet: Bool = false
+    
+    @State var statusSelected: String = Constants.statusOptions[0]
+    
+    @State var locationSelected: String = ""
+    
+    
+    @State private var isEdit: Bool = false
+    
+    @StateObject var viewModel: DetailAccessoryViewModel = DetailAccessoryViewModel()
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: true) {
+            VStack(spacing: 10) {
+                Group {
+                    TitleSubForm(title: "Accessories Information")
+                        .hLeading()
+                    
+                    CustomTextField(title: "Name", text: self.$name, iconName: "person", strokeColor: isEdit ? .secondaryAccent : .primaryGray)
+                        .disabled(!isEdit)
+                    
+                    CustomTextField(title: "Price", text: self.$price,keyboardType: .numberPad, iconName: "tag",strokeColor: isEdit ? .secondaryAccent : .primaryGray)
+                        .disabled(!isEdit)
+                    
+                    CustomTextField(title: "Stock", text: self.$stock,keyboardType: .numberPad, iconName: "shippingbox",strokeColor: isEdit ? .secondaryAccent : .primaryGray)
+                        .disabled(!isEdit)
+                }
+                
+                Group {
+                    TitleSubForm(title: "Status")
+                        .hLeading()
+                        .padding(.top, 16)
+                    CustomSegmentedControl(selectedItem: self.$statusSelected, items: Constants.statusOptions, strokeColor: isEdit ? .secondaryAccent : .primaryGray)
+                        .disabled(!isEdit)
+                    
+                }
+                
+                Group {
+                    TitleSubForm(title: "Location")
+                        .hLeading()
+                        .padding(.top, 16)
+                    
+                    CustomMenuPicker(menuItemSelection: self.$locationSelected, menus: Constants.locationOptions, title: "", strokeColor: isEdit ? .secondaryAccent : .primaryGray)
+                        .disabled(!isEdit)
+                }
+                
+                Group {
+                    TitleSubForm(title: "Image")
+                        .hLeading()
+                        .padding(.top, 16)
+                    
+                    Button {
+                        self.showImageActionDialog = true
+                    } label: {
+                        if currentImage == nil {
+                            AddImageSubForm(strokeColor: isEdit ? .secondaryAccent : .primaryGray)
+                        } else {
+                            Image(uiImage: self.currentImage!)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(maxWidth: .infinity, minHeight: 120)
+                                .clipped()
+                                .cornerRadius(8)
+                                .padding(4)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(isEdit ? Color.secondaryAccent : Color.primaryGray, style: StrokeStyle(lineWidth: 2, dash: [10]))
+                                }
+                        }
+                    }
+                    .disabled(!isEdit)
+                    
+                }
+                
+                Spacer()
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal, 16)
+        }
+        .onAppear {
+            print(imageUrl)
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle("Detail Accessory")
+        
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    if isEdit {
+                        
+                        if let price = Double(self.price), let stock = Int(self.stock),  let image = self.currentImage {
+                            
+                            viewModel.updateAccessory(id: self.id, name: self.name, addedAt: Date(), price: price, stock: stock, location: self.locationSelected, status: self.statusSelected, imageUrl: imageUrl, image: image) { result in
+                                switch(result){
+                                case .success:
+                                    showAlert(isActive: true, title: "Success", message: "Accessory has been updated")
+                                case .failure(_):
+                                    showAlert(isActive: true, title: "Failed", message: "An error occured when update the data")
+                                }
+                            }
+                        }
+                        
+                        isEdit.toggle()
+                    } else {
+                        print("Edit")
+                        isEdit.toggle()
+                    }
+                } label: {
+                    Text(isEdit ? "Save" : "Edit")
+                }
+                .tint(isEdit ? .primaryAccent : .secondaryAccent)
+                .disabled(!isFormValid())
+                .alert(self.alertTitle, isPresented: self.$showAlert, actions: {
+                    Button("OK") {
+                        if self.alertTitle == "Success"{
+                            self.presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                }, message: {
+                    Text(self.alertMessage)
+                })
+            }
+        }
+        
+        .confirmationDialog("Choose Action To Do", isPresented: self.$showImageActionDialog){
+            
+            Button("Galeri"){
+                self.showImagePickerSheet = true
+                self.sourceType = .photoLibrary
+            }
+            
+            Button("Kamera") {
+                self.showImagePickerSheet = true
+                self.sourceType = .camera
+            }
+            
+            Button("Hapus Foto", role: .destructive) {
+                self.currentImage = nil
+            }
+            
+            Button("Batal", role: .cancel){
+                
+            }
+            
+        } message: {
+            Text("Choose Action To Do")
+        }
+        .sheet(isPresented: self.$showImagePickerSheet){
+            ImagePicker(image: self.$currentImage, isShown: self.$showImagePickerSheet, sourceType: self.sourceType)
+        }
     }
+    
+    private func showAlert(isActive: Bool, title: String ,message: String){
+        self.showAlert = isActive
+        self.alertMessage = message
+        self.alertTitle = title
+    }
+    
+    
+    
+    func isFormValid() -> Bool {
+        if name.isEmpty {
+            return false
+        }
+        
+        if locationSelected.isEmpty {
+            return false
+        }
+        
+        if statusSelected.isEmpty {
+            return false
+        }
+        
+        if let _ = Double(self.price), let _ = Int(self.stock), let _ = self.currentImage {
+            return true
+        } else {
+            return false
+        }
+    }
+    
 }
 
 struct DetailAccessoryView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailAccessoryView()
+        DetailAccessoryView(id: "", imageUrl: "", addedAt: Date())
     }
 }
+
--- e-weapon/Page/DetailAccessory/DetailAccessoryViewModel.swift
+++ e-weapon/Page/DetailAccessory/DetailAccessoryViewModel.swift
@@ -11,7 +11,7 @@ class DetailAccessoryViewModel: ObservableObject {
     
     private let databaseManager = DatabaseManager.shared
     
-    func updateWeapon(id: String, name: String, addedAt: Date, price: Double, stock: Int, location: String, status: String, imageUrl: String, image: UIImage , completion: @escaping (Result<Void, Error>) -> Void){
+    func updateAccessory(id: String, name: String, addedAt: Date, price: Double, stock: Int, location: String, status: String, imageUrl: String, image: UIImage , completion: @escaping (Result<Void, Error>) -> Void){
         
         databaseManager.updateAccessory(id: id, name: name, addedAt: addedAt, price: price, stock: stock, location: location, status: status, imageUrl: imageUrl, image: image, completion: completion)
     }
