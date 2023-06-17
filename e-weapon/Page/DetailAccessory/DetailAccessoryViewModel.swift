f905a295137bd4fe6b98aba3d95a636d3bb9d821
rivaldofez <rivaldofez@gmail.com>
2023-06-17 15:19:51 +0700

    implement update accessory on view model

--- e-weapon/Page/DetailAccessory/DetailAccessoryViewModel.swift
+++ e-weapon/Page/DetailAccessory/DetailAccessoryViewModel.swift
@@ -5,4 +5,16 @@
 //  Created by Rivaldo Fernandes on 17/06/23.
 //
 
-import Foundation
+import UIKit
+
+class DetailAccessoryViewModel: ObservableObject {
+    
+    private let databaseManager = DatabaseManager.shared
+    
+    func updateWeapon(id: String, name: String, addedAt: Date, price: Double, stock: Int, location: String, status: String, imageUrl: String, image: UIImage , completion: @escaping (Result<Void, Error>) -> Void){
+        
+        databaseManager.updateAccessory(id: id, name: name, addedAt: addedAt, price: price, stock: stock, location: location, status: status, imageUrl: imageUrl, image: image, completion: completion)
+    }
+    
+}
+
