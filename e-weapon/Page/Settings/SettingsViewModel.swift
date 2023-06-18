94d0c8751f78e97718b4d04498584ffd18fafcce
rivaldofez <rivaldofez@gmail.com>
2023-06-18 23:55:45 +0700

    add function to set theme style to user defaults

--- e-weapon/Page/Settings/SettingsViewModel.swift
+++ e-weapon/Page/Settings/SettingsViewModel.swift
@@ -5,8 +5,16 @@
 //  Created by Rivaldo Fernandes on 18/06/23.
 //
 
-import Foundation
+import SwiftUI
 
 class SettingsViewModel: ObservableObject {
     
+    @Published var currentThemeStyle: Bool = true
+    
+    func setThemeStyle(isDark: Bool){
+        UserDefaults.standard.set(isDark, forKey: "theme")
+        (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first!.overrideUserInterfaceStyle = isDark ? .dark : .light
+        currentThemeStyle = isDark
+        
+    }
 }
