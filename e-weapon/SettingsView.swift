398c5b3ecbd56edf49903770b3223c6c45704245
rivaldofez <rivaldofez@gmail.com>
2023-06-18 23:38:35 +0700

    setup dark mode color set

--- e-weapon/SettingsView.swift
+++ e-weapon/SettingsView.swift
@@ -10,7 +10,82 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        VStack {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Theme Preference")
+                    .font(.system(.body).bold())
+                    .padding()
+                    .foregroundColor(.primaryAccent)
+                
+                Divider()
+                
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Image(systemName: "sun.max")
+                            .resizable()
+                            .frame(width:
+                                    30, height: 30)
+                            .foregroundColor(.primaryLabel)
+                        Text("Light Mode")
+                            .font(.system(.body).weight(.medium))
+                            .padding(.leading, 16)
+                            .foregroundColor(.primaryLabel)
+                        
+                        Spacer()
+                        
+                        
+                        Image(systemName: "checkmark")
+                            .font(.system(.body).weight(.bold))
+                            .foregroundColor(.primaryAccent)
+                            .padding(.horizontal)
+                        
+                        
+                    }.padding()
+                }
+                
+                Divider()
+                
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Image(systemName: "moon")
+                            .resizable()
+                            .frame(width:
+                                    30, height: 30)
+                            .foregroundColor(.primaryLabel)
+                        Text("Dark Mode")
+                            .font(.system(.body).weight(.medium))
+                            .padding(.leading, 16)
+                            .foregroundColor(.primaryLabel)
+                        
+                        Spacer()
+                        
+                        
+                        Image(systemName: "checkmark")
+                            .font(.system(.body).weight(.bold))
+                            .foregroundColor(.primaryAccent)
+                            .padding(.horizontal)
+                        
+                    }
+                    .padding()
+                }
+                
+            }
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.primaryGray.opacity(0.3), lineWidth: 1)
+            )
+            .padding(.horizontal)
+        }
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Text("Settings")
+                    .font(.system(.title).bold())
+            }
+        }
     }
 }
 
