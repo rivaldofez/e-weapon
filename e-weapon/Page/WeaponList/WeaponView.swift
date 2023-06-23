87d96f8898f888594cf5b8fd6196ee0c72cff0e3
rivaldofez <rivaldofez@gmail.com>
2023-06-24 03:42:27 +0700

    implement generate excel file to view model

--- e-weapon/Page/WeaponList/WeaponView.swift
+++ e-weapon/Page/WeaponList/WeaponView.swift
@@ -28,29 +28,34 @@ struct WeaponView: View {
         NavigationView {
             VStack(spacing: 0) {
                 Button("Test XLSX"){
-//                    ExportXlsxService().export()
-                    generateExcelFile(weapons: viewModel.weapon)
-    
+                    //                    ExportXlsxService().export()
+//                    generateExcelFile(weapons: viewModel.weapon)
+                    
+                    viewModel.generateExcelFile()
+                    if(!viewModel.documentItemsExport.isEmpty){
+                        shareSheet = true
+                    }
+                    
                 }
                 .popover(isPresented: $shareSheet) {
-                    ShareSheetView(items: $items)
+                    ShareSheetView(items: $viewModel.documentItemsExport)
                 }
                 
                 
-                if viewModel.weapon.isEmpty {
-                        LottieView(name: "Empty", loopMode: .loop)
-                            .frame(maxHeight: 240)
-                        
-                        Text("Oops, data empty or not found")
+                if viewModel.weapons.isEmpty {
+                    LottieView(name: "Empty", loopMode: .loop)
+                        .frame(maxHeight: 240)
+                    
+                    Text("Oops, data empty or not found")
                         .font(.system(.title3).bold())
                         .foregroundColor(.primaryLabel)
                         .padding(.top, -16)
                         .padding(.horizontal, 16)
                 } else {
                     List {
-                        ForEach($viewModel.weapon, id: \.id){ $weapon in
+                        ForEach($viewModel.weapons, id: \.id){ $weapon in
                             NavigationLink {
-                                DetailWeaponView(id: weapon.id, imageUrl: weapon.imageUrl , addedAt: weapon.addedAt ,name: weapon.name, price: "\(weapon.price)", stock: "\(weapon.stock)", currentImage: getImage(imageUrl: weapon.imageUrl), statusSelected: weapon.status, locationSelected: weapon.location)
+                                DetailWeaponView(id: weapon.id, imageUrl: weapon.imageUrl , addedAt: weapon.addedAt ,name: weapon.name, price: "\(weapon.price)", stock: "\(weapon.stock)", currentImage: Helper.getImage(imageUrl: weapon.imageUrl), statusSelected: weapon.status, locationSelected: weapon.location)
                             } label: {
                                 WeaponItemView(weapon: $weapon)
                                     .hLeading()
@@ -118,7 +123,7 @@ struct WeaponView: View {
                             var number = 0
                             var csvHead = "No,Name,Price,Stock,Status,Location\n"
                             
-                            for weaponItem in viewModel.weapon {
+                            for weaponItem in viewModel.weapons {
                                 number += 1
                                 csvHead.append("\(number),\(weaponItem.name),\(weaponItem.price),\(weaponItem.stock),\(weaponItem.status),\(weaponItem.location)\n")
                             }
@@ -138,14 +143,14 @@ struct WeaponView: View {
                                 
                             }
                         }
-//                        .fileExporter(isPresented: $showFileExported, document: csvDocument, contentType: .commaSeparatedText, defaultFilename: generateFileExportName()) { result in
-//                            switch result {
-//                            case .success(let url):
-//                                print("Saved to \(url)")
-//                            case .failure(let error):
-//                                print(error.localizedDescription)
-//                            }
-//                        }
+                        //                        .fileExporter(isPresented: $showFileExported, document: csvDocument, contentType: .commaSeparatedText, defaultFilename: generateFileExportName()) { result in
+                        //                            switch result {
+                        //                            case .success(let url):
+                        //                                print("Saved to \(url)")
+                        //                            case .failure(let error):
+                        //                                print(error.localizedDescription)
+                        //                            }
+                        //                        }
                     }
                 }
             }
@@ -159,48 +164,13 @@ struct WeaponView: View {
         return "Weapon \(formatter.string(from: Date.now))"
     }
     
-    func getImage(imageUrl: String) -> UIImage {
-        let imagesDefaultURL = URL(fileURLWithPath: "/images/")
-        let imagesFolderUrl = try! FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: imagesDefaultURL, create: true)
-        let imageUrl = imagesFolderUrl.appendingPathComponent(imageUrl)
-        
-        do {
-            let imageData = try Data(contentsOf: imageUrl)
-            
-            if let imageResult = UIImage(data: imageData){
-                return imageResult
-            }
-        } catch {
-            print("Not able to load image")
-        }
-        
-        return UIImage(systemName: "exclamationmark.triangle.fill")!
-        
-    }
-    
     private func docDirectoryPath() -> String{
-        let dirPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory,
                                                            .userDomainMask,
                                                            true)
         return dirPaths[0]
     }
     
-    private func minRatio(left: (Double, Double), right: (Double, Double)) -> Double {
-        min(left.0 / right.0, left.1 / right.1)
-    }
-    
-    /// Update NSData to buffer for xlsxwriter
-    private func getArrayOfBytesFromImage(imageData: NSData) -> [UInt8] {
-        //Determine array size
-        let count = imageData.length / MemoryLayout.size(ofValue: UInt8())
-        //Create an array of the appropriate size
-        var bytes = [UInt8](repeating: 0, count: count)
-        //Copy image data as bytes into the array
-        imageData.getBytes(&bytes, length:count * MemoryLayout.size(ofValue: UInt8()))
-
-        return bytes
-    }
-        
     
     private func generateExcelFile(weapons: [Weapon]){
         let filename = "export_database.xlsx"
@@ -208,7 +178,7 @@ struct WeaponView: View {
         //cell size
         let cell_width: Double = 50
         let cell_height: Double = 50
-
+        
         var workbook: UnsafeMutablePointer<lxw_workbook>?
         var worksheet: UnsafeMutablePointer<lxw_worksheet>?
         var format_header: UnsafeMutablePointer<lxw_format>?
@@ -219,17 +189,15 @@ struct WeaponView: View {
         var destination_path = docDirectoryPath()
         destination_path.append(filename)
         pathBook = destination_path
-
+        
         workbook = workbook_new(destination_path)
         worksheet = workbook_add_worksheet(workbook, nil)
-        
         
         // Add style
         format_header = workbook_add_format(workbook)
         format_set_bold(format_header)
         format_1 = workbook_add_format(workbook)
         format_set_bg_color(format_1, 0xDDDDDD)
-        
         
         //build header
         writingLine = 0
@@ -247,8 +215,6 @@ struct WeaponView: View {
         for weapon in weapons {
             numberItem+=1
             writingLine += 1
-            let lineFormat = (writingLine % 2 == 1) ? format_1 : nil //set format
-            
             
             worksheet_write_string(worksheet, writingLine, 0, "\(numberItem)", nil)
             worksheet_write_string(worksheet, writingLine, 2, weapon.name, nil)
@@ -262,23 +228,23 @@ struct WeaponView: View {
         for (index, weapon) in weapons.enumerated() {
             let row = UInt32(index + 1)
             worksheet_set_row(worksheet, row, Double(cell_height), nil)
-            let image = getImage(imageUrl: weapon.imageUrl)
-                var options = lxw_image_options()
-                // Pixel size is Point size x image scale
-                let imageScale = image.scale
-                let uiimageSizeInPixel = (Double(image.size.width * imageScale), Double(image.size.height * imageScale))
-                let scale = minRatio(left: (cell_width, cell_height),
-                                     right: uiimageSizeInPixel )
-                options.x_offset = 10
-                options.y_offset = 1
-                options.x_scale = scale
-                options.y_scale = scale
-                options.object_position = 1
-                
-                if let nsdata = image.jpegData(compressionQuality: 0.9) as NSData? {
-                    let buffer = getArrayOfBytesFromImage(imageData: nsdata)
-                    worksheet_insert_image_buffer_opt(worksheet, row, 1, buffer, buffer.count, &options)
-                }
+            let image = Helper.getImage(imageUrl: weapon.imageUrl)
+            var options = lxw_image_options()
+            // Pixel size is Point size x image scale
+            let imageScale = image.scale
+            let uiimageSizeInPixel = (Double(image.size.width * imageScale), Double(image.size.height * imageScale))
+            let scale = Helper.minRatio(left: (cell_width, cell_height),
+                                        right: uiimageSizeInPixel )
+            options.x_offset = 10
+            options.y_offset = 1
+            options.x_scale = scale
+            options.y_scale = scale
+            options.object_position = 1
+            
+            if let nsdata = image.jpegData(compressionQuality: 0.9) as NSData? {
+                let buffer = Helper.getArrayOfBytesFromImage(imageData: nsdata)
+                worksheet_insert_image_buffer_opt(worksheet, row, 1, buffer, buffer.count, &options)
+            }
             
         }
         
@@ -290,8 +256,6 @@ struct WeaponView: View {
         
         shareSheet = true
         print(pathBook)
-        
-        
     }
 }
 
--- e-weapon/Page/WeaponList/WeaponViewModel.swift
+++ e-weapon/Page/WeaponList/WeaponViewModel.swift
@@ -6,17 +6,19 @@
 //
 
 import UIKit
+import xlsxwriter
 
 
 class WeaponViewModel: ObservableObject {
     
     private let databaseManager = DatabaseManager.shared
     
-    @Published var weapon: [Weapon] = []
+    @Published var weapons: [Weapon] = []
+    @Published var documentItemsExport: [Any] = []
     
     func fetchWeapon(){
         
-        self.weapon = databaseManager.fetchWeapon()
+        self.weapons = databaseManager.fetchWeapon()
             .map {
                 return Weapon(id: $0.id, name: $0.name, addedAt: $0.addedAt, price: $0.price, stock: $0.stock, imageUrl: $0.imageUrl, location: $0.location, status: $0.status)
             }
@@ -27,16 +29,109 @@ class WeaponViewModel: ObservableObject {
             fetchWeapon()
         } else {
             fetchWeapon()
-            self.weapon = self.weapon.filter { $0.name.lowercased().contains(query.lowercased()) }
+            self.weapons = self.weapons.filter { $0.name.lowercased().contains(query.lowercased()) }
         }
     }
     
     func deleteWeapon(id: String, completion: @escaping (Result<Void, Error>) -> Void){
-        self.weapon.removeAll { $0.id == id}
-        print(weapon)
-        print(weapon.count)
+        self.weapons.removeAll { $0.id == id}
+        print(weapons)
+        print(weapons.count)
         
         databaseManager.deleteWeapon(id: id, completion: completion)
+    }
+    
+    func generateFileExportName() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-dd-MM"
+        return "Weapon \(formatter.string(from: Date.now))"
+    }
+    
+    private func generateDocDirectory() -> String{
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+                                                           .userDomainMask,
+                                                           true)
+        return dirPaths[0].appending("/")
+    }
+    
+    func generateExcelFile(){
+        let filename = generateFileExportName() + ".xlsx"
+                
+        var workbook: UnsafeMutablePointer<lxw_workbook>?
+        var worksheet: UnsafeMutablePointer<lxw_worksheet>?
+        var format_header: UnsafeMutablePointer<lxw_format>?
+        var format_1: UnsafeMutablePointer<lxw_format>?
+        
+        
+        
+        var destination_path = generateDocDirectory()
+        destination_path.append(filename)
+        
+        workbook = workbook_new(destination_path)
+        worksheet = workbook_add_worksheet(workbook, nil)
+        
+        // Add style
+        format_header = workbook_add_format(workbook)
+        format_set_bold(format_header)
+        format_1 = workbook_add_format(workbook)
+        format_set_bg_color(format_1, 0xDDDDDD)
+        
+        //cell size
+        let cell_width: Double = 50
+        let cell_height: Double = 50
+        var writingLine: UInt32 = 0
+        
+        //build header
+        writingLine = 0
+        let format = format_header
+        format_set_bold(format)
+        worksheet_write_string(worksheet, writingLine, 0, "No", format)
+        worksheet_write_string(worksheet, writingLine, 1, "Image", format)
+        worksheet_write_string(worksheet, writingLine, 2, "Name", format)
+        worksheet_write_string(worksheet, writingLine, 3, "Price", format)
+        worksheet_write_string(worksheet, writingLine, 4, "Stock", format)
+        worksheet_write_string(worksheet, writingLine, 5, "Status", format)
+        worksheet_write_string(worksheet, writingLine, 6, "Location", format)
+        
+        for weapon in weapons {
+            writingLine += 1
+            
+            worksheet_write_string(worksheet, writingLine, 0, "\(writingLine - 1)", nil)
+            worksheet_write_string(worksheet, writingLine, 2, weapon.name, nil)
+            worksheet_write_number(worksheet, writingLine, 3, weapon.price, nil)
+            worksheet_write_number(worksheet, writingLine, 4, Double(weapon.stock), nil)
+            worksheet_write_string(worksheet, writingLine, 5, weapon.status, nil)
+            worksheet_write_string(worksheet, writingLine, 6, weapon.location, nil)
+            
+        }
+        
+        for (index, weapon) in weapons.enumerated() {
+            let row = UInt32(index + 1)
+            worksheet_set_row(worksheet, row, Double(cell_height), nil)
+            let image = Helper.getImage(imageUrl: weapon.imageUrl)
+            var options = lxw_image_options()
+            
+            // Pixel size is Point size x image scale
+            let imageScale = image.scale
+            let uiimageSizeInPixel = (Double(image.size.width * imageScale), Double(image.size.height * imageScale))
+            let scale = Helper.minRatio(left: (cell_width, cell_height),
+                                        right: uiimageSizeInPixel )
+            options.x_offset = 10
+            options.y_offset = 1
+            options.x_scale = scale
+            options.y_scale = scale
+            options.object_position = 1
+            
+            if let nsdata = image.jpegData(compressionQuality: 0.9) as NSData? {
+                let buffer = Helper.getArrayOfBytesFromImage(imageData: nsdata)
+                worksheet_insert_image_buffer_opt(worksheet, row, 1, buffer, buffer.count, &options)
+            }
+            
+        }
+        workbook_close(workbook)
+        
+        let docURL = URL(fileURLWithPath: destination_path)
+        self.documentItemsExport = [docURL as Any]
     }
     
 }
