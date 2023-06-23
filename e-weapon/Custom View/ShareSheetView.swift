8a94e74c3886ce6bebb92ffa360cec20d785694a
rivaldofez <rivaldofez@gmail.com>
2023-06-24 03:12:53 +0700

    add completion handler after share item

--- e-weapon/Custom View/ShareSheetView.swift
+++ e-weapon/Custom View/ShareSheetView.swift
@@ -14,7 +14,17 @@ struct ShareSheetView: UIViewControllerRepresentable {
     @Binding var items: [Any]
 
         func makeUIViewController(context: UIViewControllerRepresentableContext<ShareSheetView>) -> UIActivityViewController {
-            UIActivityViewController(activityItems: items, applicationActivities: nil)
+            
+            var controller = UIActivityViewController(activityItems: items, applicationActivities: nil)
+            
+            controller.completionWithItemsHandler = { (activityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) -> Void in
+                if completed == true {
+                    if let url = items.first as? URL {
+                        try! FileManager.default.removeItem(at: url)
+                    }
+                }
+            }
+            return controller
         }
 
         func updateUIViewController(_ uiViewController: UIActivityViewController, context: UIViewControllerRepresentableContext<ShareSheetView>) {
