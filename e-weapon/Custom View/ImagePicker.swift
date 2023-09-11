97c3678ec27f72b613fe048fed7147bafd0c38d8
rivaldofez <rivaldofez@gmail.com>
2023-09-11 14:24:18 +0700

    update image picker selection

--- e-weapon/Custom View/ImagePicker.swift
+++ e-weapon/Custom View/ImagePicker.swift
@@ -24,13 +24,11 @@ class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImageP
             image = uiImage
             isShown = false
         }
-        
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         isShown = false
     }
-    
 }
 
 
